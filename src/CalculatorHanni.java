e08dd0124586a8c7e95fa37fe45b2fc2a763903e
HanniSalman <124783343+HanniSalman@users.noreply.github.com>
2023-08-23 10:35:18 +0200

    Finished

--- src/CalculatorHanni.java
+++ src/CalculatorHanni.java
@@ -1,2 +1,15 @@
+
 public class CalculatorHanni {
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+
+    public double add(double a, double b) {
+        return a + b;
+    }
 }
